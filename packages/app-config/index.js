e3a46ef76f1c4c5b787a05417eb263decf9f381d
HotPotatoC <juandotulung@gmail.com>
2021-09-22 19:34:51 +0700

    refactor(app-config): added more exports

--- packages/app-config/index.js
+++ packages/app-config/index.js
@@ -11,31 +11,32 @@ dotenv.config({
   path: path.join(__dirname, !__isProd__ ? '.env' : '.env.production'),
 })
 
-const databaseHost = process.env.MYSQL_HOST || 'localhost'
-const databasePort = process.env.MYSQL_PORT || '3306'
-const databaseUser = process.env.MYSQL_USER || 'root'
-const databasePassword = process.env.MYSQL_PASSWORD || ''
-const databaseName = process.env.MYSQL_NAME || 'test'
+export const app = {
+  name: pkg.name,
+  version: pkg.version,
+  port: __isTest__ ? 0 : process.env.PORT || 5000,
+  env: process.env.NODE_ENV || 'development',
+  baseUrl: process.env.BASE_URL || `http://localhost:${process.env.PORT || 5000}`,
+}
+
+export const logger = {
+  level: __isProd__ ? 'error' : 'debug',
+  // Path is for production only
+  path: '/var/log/app.log',
+}
+
+export const database = {
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || '3306',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || '',
+  database: process.env.MYSQL_NAME || 'test',
+}
+
+database.url = `mysql://${database.user}:${database.password}@${database.host}:${database.port}/${database.database}`
 
 export default {
-  app: {
-    name: pkg.name,
-    version: pkg.version,
-    port: __isTest__ ? 0 : process.env.PORT || 5000,
-    env: process.env.NODE_ENV || 'development',
-    baseUrl: process.env.BASE_URL || `http://localhost:${process.env.PORT || 5000}`,
-  },
-  logger: {
-    level: __isProd__ ? 'error' : 'debug',
-    // Path is for production only
-    path: '/var/log/app.log',
-  },
-  database: {
-    host: databaseHost,
-    port: databasePort,
-    user: databaseUser,
-    password: databasePassword,
-    database: databaseName,
-    url: `mysql://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}`,
-  },
+  app,
+  logger,
+  database,
 }
